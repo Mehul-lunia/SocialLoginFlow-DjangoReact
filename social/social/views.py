04d0e53c99eddb3d150885041c7466c9f1eb7b15
Mehul Lunia <mehulrajlunia@gmail.com>
2023-08-24 10:13:01 +0530

    new change

--- social/social/views.py
+++ social/social/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from allauth.socialaccount.views import ConnectionsView
 
 
 @api_view(['GET'])
 def home(request):
     currUser = request.user.username
+    print(request.user.socialaccount_set.all()[0].extra_data)
     response = Response({'msg':currUser})
     return response
