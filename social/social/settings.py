6c404a08f56f013f3e2a9cea439c9d8fd2441cea
Mehul Lunia <mehulrajlunia@gmail.com>
2023-08-25 13:40:41 +0530

    created the basis for users to upload profile photo and view them in frontend through media urls

--- social/social/settings.py
+++ social/social/settings.py
@@ -182,7 +182,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+# AUTH_USER_MODEL = os.path.join(PROJECT_DIR,'OverriddingUserModel','CustomUsermodel','CustomUserModel')
+
+MEDIA_ROOT = os.path.join(PROJECT_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- social/social/urls.py
+++ social/social/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .views import get_user_information_from_request
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('accounts/', include('allauth.urls')),
-     path('',get_user_information_from_request)
-]
+    path('accounts/', include('allauth.urls')),
+    path('',get_user_information_from_request)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
